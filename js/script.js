fbb09736d6c3f3904e8bd04f8e8a3aa62878c29b
tlataste <tlataste@expertime.com>
2017-04-12 21:21:00 +0200

    scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-	$(document).ready(function() {
-		$('#fullpage').fullpage();
+$(document).ready(function(){
+
+
+	$(window).scroll(function() {
+		if ($('.section-2').visible(true)) {
+		    console.log('visible');
+		}
 	});
+
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+	   		if ($('.section-2').visible(true)) {
+			    console.log('near bottom!');
+			    $('.section-2 .map-container').addClass('active');
+			}
+	   }
+	});
+
+
+});
