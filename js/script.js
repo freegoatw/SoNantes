5a2c0557194b26c5da9d75f4feeb8a8c4b991315
tlataste <tlataste@expertime.com>
2017-04-13 19:16:19 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
 	$(window).scroll(function() {
-	   if($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+	   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
 	   		if ($('.section-2').visible(true)) {
 			    $('.section-2 .map-container').addClass('active');
 			}
 	   }
-	   if($(window).scrollTop() + $(window).height() < $(document).height() - 10) {
+	   if($(window).scrollTop() + $(window).height() < $(document).height() - 100) {
 	   		if ($('.section-2').visible(true)) {
 			    $('.section-2 .map-container').removeClass('active');
 			}
